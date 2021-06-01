3e370a0ce61ffbe6283e590cb70814b1ee9dc1d8
周睿祥 <1215686977@qq.com>
2021-06-01 09:40:40 +0800

    Search the majority element，moore algorithm!

---
+++ LeetCode/src/com/leetcode/array/repeat_element/Solution.java
@@ -0,0 +1,38 @@
+package com.leetcode.array.repeat_element;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+/**
+ * @author BlueStar
+ * @date 2021/6/1 9:18
+ * @Description: Whether the array has duplicate elements
+ */
+public class Solution {
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4, 3};
+        boolean flag = containsDuplicate(nums);
+        System.out.println(flag);
+    }
+
+    public static boolean containsDuplicate(int[] nums) {
+        // time: O(n)   space: O(n)
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int num : nums) {
+            if (!set.contains(num)) {
+                set.add(num);
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+
+
+
+
+}
