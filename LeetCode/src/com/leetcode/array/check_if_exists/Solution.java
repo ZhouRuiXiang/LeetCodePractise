7589af53bcb29cb5f332ea15b6ad669e372367d8
周睿祥 <1215686977@qq.com>
2021-06-05 08:45:16 +0800

    Check the existence of integers and their multiples

--- LeetCode/src/com/leetcode/array/check_if_exists/Solution.java
+++ LeetCode/src/com/leetcode/array/check_if_exists/Solution.java
@@ -6,6 +6,14 @@ import java.util.HashSet;
  * @author BlueStar
  * @date 2021/6/5 8:23
  * @Description: Check the existence of integers and their multiples
+ *
+ * 输入：arr = [7,1,14,11]
+ * 输出：true
+ * 解释：N = 14 是 M = 7 的两倍，即 14 = 2 * 7 。
+ *
+ * 输入：arr = [3,1,7,11]
+ * 输出：false
+ * 解释：在该情况下不存在 N 和 M 满足 N = 2 * M 。
  */
 public class Solution {
     public static void main(String[] args) {
